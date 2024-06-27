cd0db8fa96183d47c4f627a5e44c7a35fca192b5
s-n-park <146785798+s-n-park@users.noreply.github.com>
2024-06-27 16:24:54 -0700

    Cleanup psql readme

--- tools/database.py
+++ tools/database.py
@@ -2,25 +2,6 @@ from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, 
 from sqlalchemy.exc import SQLAlchemyError
 from langchain.tools import StructuredTool, tool
 
-# import os
-
-# # Locate PostgreSQL installation prefix
-# postgres_prefix = os.popen("brew --prefix postgresql").read().strip()
-
-# # Set environment variables
-# os.environ['DYLD_FALLBACK_LIBRARY_PATH'] = f"{postgres_prefix}/lib:/usr/local/lib:/usr/lib"
-# os.environ['PATH'] = f"{postgres_prefix}/bin:" + os.environ['PATH']
-# os.environ['LDFLAGS'] = f"-L{postgres_prefix}/lib"
-# os.environ['CPPFLAGS'] = f"-I{postgres_prefix}/include"
-# os.environ['DYLD_LIBRARY_PATH'] = f"{postgres_prefix}/lib:" + os.environ.get('DYLD_LIBRARY_PATH', '')
-
-# # Verify environment variables (optional, for debugging purposes)
-# print(os.environ['DYLD_FALLBACK_LIBRARY_PATH'])
-# print(os.environ['PATH'])
-# print(os.environ['LDFLAGS'])
-# print(os.environ['CPPFLAGS'])
-# print(os.environ['DYLD_LIBRARY_PATH'])
-
 def create_db_engine(db_name='db',user_name='username',password='password'):
     DATABASE_URI = f'postgresql://{user_name}:{password}@localhost:5432/{db_name}'
     try:
